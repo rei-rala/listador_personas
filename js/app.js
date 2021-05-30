19c61b5d6c31c7e5b78f83641ad0693ebe2d31be
rei-rala <ramonirala@outlook.com>
2021-05-30 01:34:49 -0300

    v1.1.1

--- js/app.js
+++ js/app.js
@@ -2,85 +2,112 @@ let listed = [];
 const counter = document.getElementById('contador');
 const target_list = document.getElementById('TARGET');
 
+const reset_link = document.getElementById('reset_link');
 const button_add = document.getElementById('btn_add');
 const button_add2 = document.getElementById('btn_add2');
 
-btn_add.addEventListener('click', pushItem );
-btn_add2.addEventListener('click', pushItem );
+reset_link.addEventListener('click', show );
 
-button_add.innerText = '+';
-button_add2.innerText = 'Agregar';
-
+const ingreso = document.getElementById('ingreso');
+ingreso.addEventListener('click', tryPush)
 
 isOverflown = element => element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
 
-show = () => {
-  let show = '';
-  if (listed.length > 0) {
-    for (let i=0; i < listed.length; i++) {
-      show += listed[i]
-    }
-  } else {
-    show = '<< Vacio >>'
-  }
-  target_list.innerHTML = show;
-  counter.innerText = listed.length;
-  const items = target_list.querySelectorAll('li');
-  for (let i=0; i<items.length; i++) { 
-    items[i].addEventListener('click', delete_item)
-  }
-}
+function refreshButton(e) {
+  // console.info(e)
+  const botones = ingreso.querySelectorAll('button');
+  
+  if (isOverflown(target_list)) {
+    console.log('Maximo por pantalla');
+    botones.forEach( elemento => {
+      elemento.style.backgroundColor = 'red';
+      elemento.style.Color = 'grey';
+    });
+    button_add.innerText = '!';
+    button_add2.innerText = 'Excedido!';
 
-
-function pushItem() {
-  //console.info('Funciona adicionar')
-  if ( isOverflown(target_list) ) {
-      console.log('Maximo por pantalla');
-      button_add.innerText = '!';
-      button_add2.innerText = 'Excedido!'
-      botton_add.style.backgroundColor = 'red';
-      botton_add2.style.backgroundColor = 'red';
-      botton_add.style.Color = 'white';
-      botton_add2.style.Color = 'white';
-  } else {
+    return false;
+  }
+  else {
+    botones.forEach( elemento => {
+      elemento.style.backgroundColor = 'green';
+      elemento.style.Color = 'white';
+      }
+    );
     button_add.innerText = '+';
     button_add2.innerText = 'Agregar'
-    
-    let name_    = document.getElementById('NOMBRE');
-    let surname_ = document.getElementById('APELLIDO');   
-    if (name_.value == '' || surname_.value =='') {
-        console.log('Falta completar al menos un dato');
-      } else {
-        console.log(`Añadido: ${name_.value} ${surname_.value}.`);
-        listed.push(`<li class='item' title="Click para eliminar este elemento." value="${listed.length+1}"> ${name_.value} ${surname_.value} </li>`); 
-        
-        show();
-        name_.value = ''; surname_.value = '';
-    }
+
+    return true;
   }
 }
 
 
-function delete_item() {
-  //console.info('Funciona delete')
-  
-  const tarr = []
-  for (let i=0; i<listed.length; i++) {
-    if (listed[i].includes(' value=\"' + this.value + '\"')) {
-      console.log(`Eliminado ${this.innerText}`);
+function show (e) {
+  let show = '';
+  if (e.target.id == 'reset_link') {
+    while (target_list.lastChild) {
+      target_list.lastChild.remove();
+    }
+    listed = [];
+    console.log('Lista vaciada');
+  }
+  else {
+    if (listed.length > 0) {
+      for (let i=0; i < listed.length; i++) {
+        show += listed[i];
+      }
     }
     else {
-      //console.log(`No es eliminado index ${i+1}`)
-      tarr.push(listed[i]);
+      show = '<< Vacio >>';
     }
+    target_list.innerHTML = show;
+    counter.innerText = listed.length;
+    target_list.addEventListener('click', delete_item);
   }
-  listed = tarr;
-
-  show();
+  return refreshButton(e);
 }
 
-reset_everything = () => {
-  console.log('Lista vaciada');
-  listed = [];
-  show();
+
+function tryPush(e) {
+  //console.info('Funciona adicionar')
+  e.stopPropagation()
+  function push () {    
+      if (refreshButton()) {
+      let name_    = document.getElementById('NOMBRE');
+      let surname_ = document.getElementById('APELLIDO');   
+      if (name_.value == '' || surname_.value =='') {
+          console.log('Falta completar al menos un dato');
+      }
+      else {
+        console.log(`Añadido: ${name_.value} ${surname_.value}.`);
+        listed.push(`<li class='item' title="Click para eliminar este elemento." value="${listed.length+1}"> ${name_.value} ${surname_.value} </li>`); 
+        show(e);
+        name_.value = ''; surname_.value = '';
+      }
+    }
+  }
+  e.target.id.includes('btn_add') ? push() : {};
+}
+
+
+function delete_item(e) {
+  //console.info('Funciona delete')
+  e.stopPropagation();
+  if (this.innerHTML == undefined) {
+    console.log(this.inner);
+  }
+  else {
+    const tarr = [];
+    for (let i=0; i<listed.length; i++) {
+      if (listed[i].includes(' value=\"' + e.target.value + '\"')) {
+        console.log(`Eliminado ${e.target.innerText}`);
+      }
+      else {
+        //console.info(`No es eliminado index ${i+1}`)
+        tarr.push(listed[i]);
+      }
+    }
+    listed = tarr;
+    show(e);
+  }
 }
